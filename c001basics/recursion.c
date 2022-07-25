0314e0145d787ae0eedd0d1785ee97fa08f21397
Iaroslav Os <osmnbx@gmail.com>
2022-07-25 19:12:19 +0300

    recursion example c/exe

--- c001basics/recursion.c
+++ c001basics/recursion.c
@@ -2,8 +2,30 @@
 
 #include <stdio.h>
 
+int factorial(int number)
+{   
+    if (number == 1){
+       return 1;
+    } else {
+        return number*factorial(number-1);
+    }
+}
+
+
+void doll(int n) {
+    if (n == 1) {
+        printf("Last doll!\n");
+    } else {
+        printf("Top side %d \n", n);
+        doll(n-1);
+        printf("Bottom side %d \n", n);
+    }
+    
+}
+
 int main(int argc, char *argv[])
 {
-    printf("Recursion\n");
+    // printf("%d ", factorial(3));
+    doll(3);
     return 0;
 }
