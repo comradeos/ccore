4f2dc2eb73f3c6e0d820088ac37519ce138c8164
Iaroslav Os <osmnbx@gmail.com>
2022-07-30 23:24:32 +0300

    updated

--- c001basics/addresses_pointers.c
+++ c001basics/addresses_pointers.c
@@ -1,12 +1,16 @@
 // gcc -o application.exe -Wall addresses_pointers.c && application.exe
 #include <stdio.h>
 
-int main(int argc, char * argv[])
-{   
-    int i = 10; // переменная
-    int * pi = &i; // указатель на переменную
-    int ** ppi = &pi; // указатель на указатель на переменную
-    int *** pppi = &ppi; // указатель на указатель на указатель на переменную
+/**
+ * @brief Демонстрация работы указателей
+ *
+ */
+void pointers_demo()
+{
+    int i = 10;         // переменная
+    int *pi = &i;       // указатель на переменную
+    int **ppi = &pi;    // указатель на указатель на переменную
+    int ***pppi = &ppi; // указатель на указатель на указатель на переменную
 
     printf("%d\n", i);
     *pi = 20; // положить значение 20 в переменную, находящуюся по адресу *pi (&i)
@@ -15,6 +19,22 @@ int main(int argc, char * argv[])
     printf("%d\n", i);
     ***pppi = 40;
     printf("%d\n", i);
+}
 
+/**
+ * @brief Демонстрация работы адресов
+ *
+ */
+void addresses_demo()
+{
+    int A[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int x = *A+9;
+    printf("%d\n", x);
+}
+
+int main(int argc, char *argv[])
+{
+    // pointers_demo();
+    addresses_demo();
     return 0;
 }
