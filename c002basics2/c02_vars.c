864698b83d4edb3a381fbd5a4bd06592ff3fb191
Iaroslav Os <osmnbx@gmail.com>
2022-08-06 23:52:40 +0300

    updatedd

--- c002basics2/c02_vars.c
+++ c002basics2/c02_vars.c
@@ -20,6 +20,16 @@ int main()
     unsigned int num5 = 4294967295; // 0 to 65,535 or 0 to 4,294,967,295
     printf("unsigned int: %u \n", num5);
 
+    long long int num6 = 922337203685477580; // -9223372036854775808 to 9223372036854775807
+    printf("long long int: %I64d \n", num6);
+
+
+
+    float num7 = 25.2;
+    printf("float: %.2f \n", num7);
+
+    double num8 = 87312.7;
+    printf("double: %.2f \n", num8);
 
     // char * my_string = "arr of chars";
     // printf("%s\n", my_string);
