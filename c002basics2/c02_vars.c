9ce4ebe5271853888492562fe84d3e10c3036a6e
Iaroslav Os <osmnbx@gmail.com>
2022-08-06 23:43:10 +0300

    updatedd

--- c002basics2/c02_vars.c
+++ c002basics2/c02_vars.c
@@ -4,11 +4,44 @@
 
 int main()
 {
-    char * my_string = "asd";
 
-    my_string = "This is my string!";
+    short num1 = 32767; // -32,768 to 32,767
+    printf("short: %d \n", num1);
 
-    printf("%s\n", my_string);
+    signed short num2 = 32767; // -32,768 to 32,767
+    printf("signed short: %d \n", num2);
+
+    unsigned short num3 = 65535; // 0 to 65,535
+    printf("unsigned short: %u \n", num3);
+
+    int num4 =  2147483647; // -32,768 to 32,767 or -2,147,483,648 to 2,147,483,647
+    printf("int: %d \n", num4);
+
+    unsigned int num5 = 4294967295; // 0 to 65,535 or 0 to 4,294,967,295
+    printf("unsigned int: %u \n", num5);
+
+
+    // char * my_string = "arr of chars";
+    // printf("%s\n", my_string);
 
     return 0;
 }
+
+
+
+
+/*
+ 
+%c	character
+%d	decimal (integer) number (base 10)
+%e	exponential floating-point number
+%f	floating-point number
+%i	integer (base 10)
+%o	octal number (base 8)
+%s	a string of characters
+%u	unsigned decimal (integer) number
+%x	number in hexadecimal (base 16)
+%%	print a percent sign
+\%	print a percent sign
+
+ */
