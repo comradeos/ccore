e6c425526cd8c02cdaf162893b7c71d2f42cce2d
Iaroslav Os <osmnbx@gmail.com>
2022-08-07 20:49:33 +0300

    updated

--- c002basics2/c04_if_else.c
+++ c002basics2/c04_if_else.c
@@ -2,14 +2,30 @@
 
 #include <stdio.h>
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
     int x = 7;
+    int y = 7;
 
-    if (x == 5) {
+    if (x == 5)
+    {
         printf("yes \n");
-    } else {
+    }
+    else
+    {
         printf("no \n");
+    }
+
+    // логическое или
+    if (x > 3 || x < 8)
+    {
+        printf("x > 3 || x < 8 \n");
+    }
+
+    // логическое и
+    if (x == 7 && y == 7)
+    {
+        printf("x == 7 && y == 7 \n");
     }
 
     return 0;
