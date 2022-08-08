e94b6c40336a41513737f3dedb7a469d72ad3aa8
Iaroslav Os <osmnbx@gmail.com>
2022-08-08 22:43:45 +0300

    added/updated

---
+++ c002basics2/c07_loops.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+
+    for(size_t i = 0; i < 10; i++) {
+        printf("%I64d ", i);
+    }
+    printf("\n\n");
+
+
+    for(float i = 100; i > 10; i /=2 ) {
+        printf("%.2f \n", i);
+    }
+    printf("\n\n");
+
+    
+    #include <stdbool.h>
+
+    bool hasCar = true;
+    int val; 
+    while (true == hasCar)
+    {
+        printf("Still has a car? 0/1 >>> ");
+        scanf("%d ", &val);
+        
+        if(val == 0) {
+            hasCar = false;
+        }
+    }
+    
+
+    return 0;
+}
