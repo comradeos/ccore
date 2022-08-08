2f8099e493eba7f721b2ceba551a7716b0f2a3f3
Iaroslav Os <osmnbx@gmail.com>
2022-08-08 23:16:40 +0300

    added/updated

--- c002basics2/c06_switch_case.c
+++ c002basics2/c06_switch_case.c
@@ -18,7 +18,7 @@ int main(int argc, char * argv[])
         printf("three \n");
         break;
     default:
-        printf("out of variants1 \n");
+        printf("out of variants \n");
         break;
     }
 
