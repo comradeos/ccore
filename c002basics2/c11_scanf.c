0a55abe589c85bd9c1fc3fedb6efefb0dc3c1225
Iaroslav Os <osmnbx@gmail.com>
2022-08-11 00:08:28 +0300

    added/updated

--- c002basics2/c11_scanf.c
+++ c002basics2/c11_scanf.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
-{
+{   
+    int A[100];
+    while (1)
+    {
+        int counter = 0;
+        int a;
+        scanf("%d", &A[counter]);
+        if (counter >= 100 || A[counter] == 0)
+        {
+            break;
+        }
+        counter++;
+    }
+
+    printf("A[0] = %d\n", A[0]);    
+    printf("A[1] = %d\n", A[1]);    
+    printf("A[2] = %d\n", A[2]);    
 
     return 0;
 
