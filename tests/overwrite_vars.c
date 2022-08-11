84d348d6d8a9388df574ecdf8dfa83d5932dfb54
Iaroslav Os <osmnbx@gmail.com>
2022-08-11 10:04:37 +0300

    added/updated

--- tests/overwrite_vars.c
+++ tests/overwrite_vars.c
@@ -17,7 +17,10 @@ int main(int argc, char *argv[])
     printf("func1(): %d\n", func1(5));
     printf("func2(): %.2f\n", func2(1));
 
-    
+    int a =123;
+    printf("a: %d\n", a); // 123
+    free(a);
+    printf("a: %d\n", a); // ничего 
 
     return 0;
 }
