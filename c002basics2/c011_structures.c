b2472ba4be5e63fe8e9002137b22d40d15eb2b2f
Iaroslav Os <osmnbx@gmail.com>
2022-08-12 18:54:43 +0300

    added/updated

--- c002basics2/c011_structures.c
+++ c002basics2/c011_structures.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 struct Car {
     int speed;
-    
+    char name[50];
+    float weight;
 };
 
 int main()
 {
-    int speed = 200;
+    struct Car bmw;
+    bmw.speed = 250;
+    bmw.name = "BMW M5";
+    // strcpy(bmw.name, "BMW M5");
+    char str[5] = "hello";
     return 0;
 }
