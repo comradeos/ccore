60f2ca6835404f2a792a1e807699f4ca25a3f6cf
Iaroslav Os <osmnbx@gmail.com>
2022-08-13 13:13:43 +0300

    added/updated

--- c002basics2/tempCodeRunnerFile.c
+++ c002basics2/tempCodeRunnerFile.c
@@ -1,9 +1,1 @@
-    fgets(line, 255, myFile);
-        printf("%s\n", line);
-
-    fgets(line, 255, myFile);
-        printf("%s\n", line);
-
-    fgets(line, 255, myFile);
-        printf("%s\n", line);
-
+* 
