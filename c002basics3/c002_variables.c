803d6ae4592a65b64d52a94a9a623489c6ef9c6d
Iaroslav Os <osmnbx@gmail.com>
2022-08-13 18:19:57 +0300

    added/updated

--- c002basics3/c002_variables.c
+++ c002basics3/c002_variables.c
@@ -4,8 +4,7 @@
 int main()
 {
     char text[] = "this is a text";
-    int * pText = &text;
 
-    printf("%s | %c | %p | %c ", text, text[0], pText, *pText);
+    printf("%p", &text);
     return 0;
 }
