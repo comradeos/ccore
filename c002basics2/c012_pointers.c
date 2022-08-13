514e098f3c9507c3a2a6a9ef9affd6be5f5c7e72
Iaroslav Os <osmnbx@gmail.com>
2022-08-13 12:12:28 +0300

    added/updated

--- c002basics2/c012_pointers.c
+++ c002basics2/c012_pointers.c
@@ -2,8 +2,16 @@
 
 int main()
 {
-    int num = 0;
-    scanf("%d", &num);
+    int num = 0; // переменная
+
+    printf("%d\n", num); // вывести значение
+    printf("%p\n", &num); // вывести адрес значения
+
+    int * pNum = &num; // указатель, содержащий адрес
+    printf("%p\n", pNum); // вывести указатель, содержащий адрес
+
+
+    // scanf("%d", &num);
     
     return 0;
 }
