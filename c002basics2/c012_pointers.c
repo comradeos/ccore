c05a41b2f987d0bf1e71c805bc0ee12b654580b5
Iaroslav Os <osmnbx@gmail.com>
2022-08-13 12:24:12 +0300

    added/updated

--- c002basics2/c012_pointers.c
+++ c002basics2/c012_pointers.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+struct Abstract
+{
+    int weight, height;
+};
+
+void square(struct Abstract object);
+
+
+
 int main()
 {
     int num = 25; // переменная
@@ -14,7 +23,17 @@ int main()
     printf("%d\n", * &num); // вывести значение через адрес
 
 
-    // scanf("%d", &num);
-    
+    struct Abstract rectangle;
+    rectangle.weight = 5;
+    rectangle.height = 7;
+    square(rectangle);
+
+
     return 0;
+}
+
+void square(struct Abstract object)
+{
+    int result = object.weight * object.height;
+    printf("result: %d\n", result);
 }
