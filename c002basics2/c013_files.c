559e4c2419e5cdd2424cba46b08620c9a8fd8796
Iaroslav Os <osmnbx@gmail.com>
2022-08-13 13:13:27 +0300

    added/updated

--- c002basics2/c013_files.c
+++ c002basics2/c013_files.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 
+void readLines(FILE * file, int linesNum);
+
 int main()
 {
     // создаем указатель на основе типа данных FILE
-    FILE * file = fopen("c014_test_file.txt", "r");
+    FILE * myFile = fopen("c014_test_file.txt", "a"); // w - перезаписать, a - дописать
+    fprintf(myFile, "Hello\n"); // записать в файл
+    fclose(myFile); // закрыть файл
+
+
+    // открыть файл для чтения
+    FILE * myFileToRead = fopen("c014_test_file.txt", "r"); // r - читать
+
+    // в определенный массив записать определенное количество символов 
+    // которые будут считаны с определенного файла
+    // fgets(имяМассива, количествоСимволов, указательФайла)
+    
+    // char line[255];
+    // fgets(line, 255, myFile);
+    // printf("%s\n", line);
+
+    readLines(myFileToRead, 8);
     return 0;
+}
+
+void readLines(FILE * file, int linesNum)
+{
+    char line[255];
+    for (size_t i = 0; i < linesNum; i++)
+    {
+        fgets(line,255,file);
+        printf("%s", line);
+    }
 }
--- c002basics2/tempCodeRunnerFile.c
+++ c002basics2/tempCodeRunnerFile.c
@@ -1,11 +1,9 @@
-    printf("\n\n\n");
+    fgets(line, 255, myFile);
+        printf("%s\n", line);
 
-    for (size_t i = 0; i <= 2; i++)
-    {
-        for (size_t j = 0; j <= 3; j++)
-        {
-            printf("%d, ", array2dim[i][j]);
-        }
-            printf("\n");
-    }
-    
+    fgets(line, 255, myFile);
+        printf("%s\n", line);
+
+    fgets(line, 255, myFile);
+        printf("%s\n", line);
+
