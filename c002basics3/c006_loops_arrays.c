892aef90fd0aace7847d8a6f5b8cc7946c840cf2
Iaroslav Os <osmnbx@gmail.com>
2022-08-14 14:27:23 +0300

    added/updated

---
+++ c002basics3/c006_loops_arrays.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    i = 1;
+    while (i <= 20)
+    {
+        printf("Hello, world!\n");
+        i = i + 1;
+    }
+    
+    return 0;
+}
