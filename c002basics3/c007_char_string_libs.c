a75c84f41444106491a19d697b80dbb4189c46a3
Iaroslav Os <osmnbx@gmail.com>
2022-08-14 14:47:40 +0300

    added/updated

--- c002basics3/c007_char_string_libs.c
+++ c002basics3/c007_char_string_libs.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h> // ббл для чтения 
 
 int main() {
     char ch = 'W';
-    
     printf("%c\n", ch);
+
+    // scanf("%c", &ch);
+    ch = getch(); // библ conio.h, возвращает символ в качестве результата, не требует нажатия enter
+    printf("ch = %c\n", ch);
+
 
     return 0;
 }
