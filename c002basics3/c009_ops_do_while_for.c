eef78c0baeb8e4eab23e7a27140daf26d0856a1e
Iaroslav Os <osmnbx@gmail.com>
2022-08-14 16:39:44 +0300

    added/updated

---
+++ c002basics3/c009_ops_do_while_for.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    int a = 22;
+    printf("a = %d\n", a);
+    
+    a++;
+    printf("a++ :%d\n", a);
+
+    a--;
+    printf("a-- :%d\n", a);
+    
+    a+=10;
+    printf("a+=10 :%d\n", a);
+    
+    a-=5;
+    printf("a-=5 :%d\n", a);
+
+    return 0;
+}
