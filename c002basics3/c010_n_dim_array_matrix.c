97e1f0bb2624120446fb628239c4b4be4300f46e
Iaroslav Os <osmnbx@gmail.com>
2022-08-20 19:23:35 +0300

    added/updated

--- c002basics3/c010_n_dim_array_matrix.c
+++ c002basics3/c010_n_dim_array_matrix.c
@@ -3,6 +3,16 @@
 
 int main()
 {
-    int m[9];
+    int m[9][9];
+    for (int i = 0; i < 9; i++)
+    {
+        for(int j = 0; j < 9; j++) 
+        {
+            m[i][j] = 7;
+            printf("%d ", m[i][j]);
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
