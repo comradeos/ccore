5c5e9ac599281a1691c2b69c19f91f9fafe27954
Iaroslav Os <osmnbx@gmail.com>
2022-08-20 22:58:32 +0300

    added/updated

--- c002basics3/c011_game_1.c
+++ c002basics3/c011_game_1.c
@@ -7,18 +7,21 @@ int main()
 {
     char mas[10][21];
 
-    sprintf(mas[0], "##########");
+    sprintf(mas[0], "####################");
     for (size_t i = 1; i < 9; i++)
     {
-        sprintf(mas[i], "#        #");
+        sprintf(mas[i], "#                  #");
     }
     
-    sprintf(mas[9], "##########");
+    sprintf(mas[9], "####################");
 
     for (size_t i = 0; i < 10; i++)
     {
         printf("%s\n", mas[i]);
     }
     
+    
+
+
     return 0;
 }
