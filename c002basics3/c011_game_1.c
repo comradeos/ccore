09d25b089874ab0ada436e47500be9e7d4e6dd29
Iaroslav Os <osmnbx@gmail.com>
2022-08-20 20:10:25 +0300

    added/updated

--- c002basics3/c011_game_1.c
+++ c002basics3/c011_game_1.c
@@ -1,7 +1,16 @@
+// https://www.youtube.com/watch?v=8WRCndazFes&list=PLBOPkQsFLCR2DWRY74L03FmbRtz_Yy73_&index=12
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
+    char mas[10][20];
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        printf("%s\n", mas[i]);
+    }
+    
     return 0;
 }
--- c002basics3/tempCodeRunnerFile.c
+++ c002basics3/tempCodeRunnerFile.c
@@ -1,17 +1,15 @@
+// https://www.youtube.com/watch?v=8WRCndazFes&list=PLBOPkQsFLCR2DWRY74L03FmbRtz_Yy73_&index=12
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    int m[9][9];
-    for (int i = 1; i <= 9; i++)
+    char mas[10][20];
+
+    for (size_t i = 0; i < 10; i++)
     {
-        for(int j = 1; j <= 9; j++) 
-        {
-            m[i][j] = (i) * (j);
-            printf("%4d", m[i][j]);
-        }
-        printf("\n");
+        printf("%s\n", mas[i]);
     }
     
     return 0;
