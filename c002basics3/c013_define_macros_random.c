90b5f147619461a35075507d8d64e65b5d72170d
Iaroslav Os <osmnbx@gmail.com>
2022-08-29 21:25:02 +0300

    added/updated

--- c002basics3/c013_define_macros_random.c
+++ c002basics3/c013_define_macros_random.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// #макрос(константа) имя значение
+#define cnt 5
+// можно создавать идентификатор для типа 
+#define celoe int
+
+#define celoeK int k = 57;
+
+#define anotherMacros printf("%s\n", "This is my macros!");
+
 int main()
 {
-    int i = 7;
+    celoe i = 7;
+    printf("%d\n", cnt);
     printf("%d\n", i);
+    celoeK;
+    printf("%d\n", k);
+
+    anotherMacros; // This is my macros!
+
     return 0;
 }
