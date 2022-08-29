3673608c1798c01b48a43a0f4669eec159ae42fd
Iaroslav Os <osmnbx@gmail.com>
2022-08-29 22:08:49 +0300

    added/updated

--- c002basics3/c014_macros_random_task.c
+++ c002basics3/c014_macros_random_task.c
@@ -8,31 +8,37 @@ int main()
 {
     printf("%d", CONST); // 1
 
-    
     int array[CONST]; // 2
-    for (size_t i = 0; i < CONST; i++) 
-    {
+    for (size_t i = 0; i < CONST; i++) {
         array[i] = i + 1; // 2
     }
-    
     printf("\n");
-
-    for (size_t i = 0; i < CONST; i++)
-    {
+    for (size_t i = 0; i < CONST; i++) {
         printf("%d ", array[i]); // 2
     }
-
     printf("\n");
 
 
 
-
+    // random between 10 and 19
     srand(time(NULL)); // 3
-    int randomNumber = (rand() % (20 - 10 + 1)) + 10;  
-    printf("%d ", randomNumber);
+    int randomNumber = (rand() % (19 - 10 + 1)) + 10; // 3
+    printf("%d ", randomNumber); // 3
 
     
+
+    srand(time(NULL)); // 4
+    int anotherRandomNumber; // 4
+    for (size_t i = 0; i < CONST; i++) {
+        anotherRandomNumber = (rand() % (39 - 20 + 1)) + 20; // 4
+        array[i] = anotherRandomNumber; // 4
+    }
+    printf("\n");
+    for (size_t i = 0; i < CONST; i++) {
+        printf("%d ", array[i]); // 2
+    }
+    printf("\n");
     
-    
+
     return 0;
 }
