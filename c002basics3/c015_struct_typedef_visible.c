dd46a97b876d369c585d6362443704dd41893baa
Iaroslav Os <osmnbx@gmail.com>
2022-09-03 11:45:43 +0300

    added/updated

--- c002basics3/c015_struct_typedef_visible.c
+++ c002basics3/c015_struct_typedef_visible.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Структура описания точки
+struct sPoint {
+    int x, y;
+};
+
 int main() {
 
+    struct sPoint point;
+    point.x = 123;
+    point.y = 567;
     
+    printf("point: x = %d, y = %d\n", point.x, point.y);
+
     return 0;
 }
