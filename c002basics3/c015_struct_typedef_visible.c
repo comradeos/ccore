cc7739f4d2208ada846bcf56c3d5752c2fcc7342
Iaroslav Os <osmnbx@gmail.com>
2022-09-03 13:30:44 +0300

    added/updated

--- c002basics3/c015_struct_typedef_visible.c
+++ c002basics3/c015_struct_typedef_visible.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Структура описания точки
 struct sPoint
@@ -50,13 +51,54 @@ int main()
 
     //
 
+    
+
     tasks();
+
     return 0;
 }
 
 void tasks()
 {
+    // 1
     struct interiorPiece
     {
+        int length, height, width;
+        char name [50];
     };
+
+    struct interiorPiece a;
+    a.length = 100;
+    a.height = 200;
+    a.width = 300;
+    strcpy(a.name, "Interior Piece");
+    
+    printf("a: length=%d, height=%d, width=%d, name=%s\n", a.length, a.height, a.width, a.name);
+
+    // 2 
+    struct interiorPiece array[3];
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        array[i].length = i + 1;
+        array[i].height = i + 2;
+        array[i].width = i + 3;
+        char a[10];
+        itoa(i, a, 0);
+        printf("%s----------\n", a);
+        strcpy(array[i].name, "Interior Piece");
+    }
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        printf("a: length=%d, height=%d, width=%d, name=%s\n", 
+            array[i].length, 
+            array[i].height, 
+            array[i].width, 
+            array[i].name
+        );
+    }
+
+
 }
+
