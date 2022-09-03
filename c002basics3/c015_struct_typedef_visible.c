4583bb1d470d97ba660d730af52edb2fb0aaae7c
Iaroslav Os <osmnbx@gmail.com>
2022-09-03 11:41:13 +0300

    added/updated

--- c002basics3/c015_struct_typedef_visible.c
+++ c002basics3/c015_struct_typedef_visible.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char const *argv[]) {
-    printf("argc: %d\n", argc);
-    printf("argv:\n");
+int main() {
 
-    for (size_t i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
-    }
     
     return 0;
 }
