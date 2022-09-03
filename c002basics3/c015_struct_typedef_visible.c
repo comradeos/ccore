67f1845ad24e6941152855cca59a4c008ff64711
Iaroslav Os <osmnbx@gmail.com>
2022-09-03 12:09:46 +0300

    added/updated

--- c002basics3/c015_struct_typedef_visible.c
+++ c002basics3/c015_struct_typedef_visible.c
@@ -16,14 +16,15 @@ int main() {
     struct sPoint point; // объявление новой структуры
     point.x = 123;
     point.y = 567;
+    printf("point: x = %d, y = %d\n", point.x, point.y);
     
     struct sPoint a; // объявление еще одной структуры
     a.x = 41;
     a.y = 23;
-    
     printf("point: x = %d, y = %d\n", a.x, a.y);
 
-
+    myType number = 23;
+    printf("myType number = %d\n", number);
 
 
     return 0;
