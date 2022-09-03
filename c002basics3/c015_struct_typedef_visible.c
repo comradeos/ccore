b16d62190310cb762d4e4678bd3a48b2b998511a
Iaroslav Os <osmnbx@gmail.com>
2022-09-03 13:36:13 +0300

    added/updated

--- c002basics3/c015_struct_typedef_visible.c
+++ c002basics3/c015_struct_typedef_visible.c
@@ -71,6 +71,7 @@ void tasks()
     a.length = 100;
     a.height = 200;
     a.width = 300;
+
     strcpy(a.name, "Interior Piece");
     
     printf("a: length=%d, height=%d, width=%d, name=%s\n", a.length, a.height, a.width, a.name);
@@ -83,10 +84,12 @@ void tasks()
         array[i].length = i + 1;
         array[i].height = i + 2;
         array[i].width = i + 3;
-        char a[10];
-        itoa(i, a, 0);
-        printf("%s----------\n", a);
-        strcpy(array[i].name, "Interior Piece");
+
+        char stringI[10];
+        itoa(i + 1, stringI, 10); // преобразовать число в строку
+
+        strcpy(array[i].name, "Interior Piece #"); // скопировать строку в массив
+        strcat(array[i].name, stringI); // дописать в массив строку
     }
 
     for (size_t i = 0; i < 3; i++)
@@ -98,6 +101,13 @@ void tasks()
             array[i].name
         );
     }
+
+
+
+    int a1 = 123;
+    char s1[100];
+    itoa(a1,s1,18);
+    printf("%s\n", s1);
 
 
 }
