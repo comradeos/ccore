cbb0f114dec45cbacb896e2f775b4fcd518953db
Iaroslav Os <osmnbx@gmail.com>
2022-09-17 16:58:16 +0300

    added/updated

--- c002basics3/c020_snow_generator.c
+++ c002basics3/c020_snow_generator.c
@@ -5,7 +5,7 @@
 #include <windows.h> // для работы с клавиатурой
 
 #define HEIGHT 25
-#define WIDTH 80
+#define WIDTH 76
 
 char array[HEIGHT][WIDTH+1];
 
@@ -14,7 +14,7 @@ void init()
     for (int i=0; i<WIDTH; i++)
         array[0][i] = ' '; // инициализация первой строки
     array[0][WIDTH] = '\0'; // символ завершения строки
-    for (int i=0; i<HEIGHT; i++)
+    for (int i=1; i<HEIGHT; i++)
         sprintf(array[i], array[0]); // копирование первой строки во все остальные
 }
 
@@ -25,22 +25,51 @@ void show()
         printf(array[i]);
 }
 
+void newSnow()
+{
+    for (int i=0; i<WIDTH; i++)
+        if (rand() % 12 == 1)
+            array[0][i] = '*';
+}
+
+void moveSnow()
+{
+    int dx;
+    for (int j=HEIGHT-1; j>=0; j--)
+        for (int i=0; i<WIDTH; i++)
+            if (array[j][i] == '*')
+            {
+                array[j][i] = ' ';
+                dx = 0;
+                if (rand() % 10 < 1) dx++;
+                if (rand() % 10 < 1) dx--;
+                dx = dx + i;
+                if ((dx >= 0) && (dx < WIDTH) && (j+1 < HEIGHT)) // только если снежинка в границах поля 
+                    array[j+1][dx] = '*';
+            }
+}
+
+
+void setCursor(int x, int y)
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
 int main()
 {
+    init();
 
-    char a[10][10];
-    
-    for (size_t i = 0; i < 10; i++)
-        a[0][i] = 'a';
-    a[0][10] = '\0';
-    
-    for (size_t i = 1; i < 10; i++)
-        sprintf(a[i], a[0]);
-    
-    for (size_t i = 0; i < 10; i++)
-        printf(a[i]);
+    do
+    {
+        newSnow();
+        moveSnow();
+        Sleep(100);
+    }
+    while(GetKeyState(VK_ESCAPE) >= 0);
 
-    // init();
-    // show();
+    show();
     return 0;
 }
