bf171b8b75ecc608e000f4baf3ac71101b653d6a
Iaroslav Os <osmnbx@gmail.com>
2022-09-17 16:12:14 +0300

    added/updated

--- c002basics3/c020_snow_generator.c
+++ c002basics3/c020_snow_generator.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <string.h>
-#include <windows.h>
+#include <string.h> // работа со строками
+#include <windows.h> // для работы с клавиатурой
+
+#define HEIGHT 25
+#define WIDTH 80
+
+char array[HEIGHT][WIDTH+1];
+
+void init()
+{
+    for (int i=0; i<WIDTH; i++)
+        array[0][i] = ' ';
+    array[0][WIDTH] = '\0';
+    for (int i=0; i<HEIGHT; i++)
+        sprintf(array[i], array[0]);
+}
 
 int main()
 {
-
+    
     return 0;
 }
