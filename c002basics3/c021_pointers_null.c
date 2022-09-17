8e2758c4c96211c5876e7163a0091d91f1d3aa27
Iaroslav Os <osmnbx@gmail.com>
2022-09-17 18:38:04 +0300

    added/updated

--- c002basics3/c021_pointers_null.c
+++ c002basics3/c021_pointers_null.c
@@ -10,9 +10,9 @@ int main()
     num = 5; // значение переменной
     *numPtr = 12; // изменяем значение через указатель (num изменился на 12)
 
-    numPtr = NULL;
-    if (numPtr != NULL)
-        *numPtr = 7;
+    numPtr = NULL; // указатель указывает никуда
+    if (numPtr != NULL) 
+        *numPtr = 7; // изменить значение если в указателе содержится адрес
 
     printf("%d\n", num);
     return 0;
