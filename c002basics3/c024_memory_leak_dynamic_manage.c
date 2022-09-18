ef109b137cf174d46984a4b22f2e0ab40526d121
Iaroslav Os <osmnbx@gmail.com>
2022-09-18 18:02:00 +0300

    added/updated

--- c002basics3/c024_memory_leak_dynamic_manage.c
+++ c002basics3/c024_memory_leak_dynamic_manage.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void tasks();
+
+
 int main()
 {
     int x;
@@ -20,8 +24,11 @@ int main()
     printf("%d\n", x); // 1 байт, 8 бит
 
     char *somePtr = NULL;
-    int pointerSize = sizeof(somePtr);
-    printf("%d\n", pointerSize); // 8 байт, 64 бита
+    if (somePtr != NULL)
+    {
+        int pointerSize = sizeof(somePtr);
+        printf("%d\n", pointerSize); // 8 байт, 64 бита
+    }
 
     // malloc - выделяет ячейку в памяти заданного размера и возвращает адрес выделенной ячейки памяти
     int a = malloc(4); // выделить 4 байта в памяти
@@ -31,12 +38,61 @@ int main()
 
 
     int *varPtr = NULL; // создаем пустой указатель
-    varPtr = malloc(sizeof(int)); // резервируем память в количестве sizeof(int) байт
-    *varPtr = 1; // обращаемся по адресу в памяти и присваиваем туда значение 1
-    printf("%d\n", *varPtr); // // обращаемся по адресу в памяти и выводим значение
+    if (varPtr != NULL)
+    {
+        varPtr = malloc(sizeof(int)); // резервируем память в количестве sizeof(int) байт
+        *varPtr = 1; // обращаемся по адресу в памяти и присваиваем туда значение 1
+        printf("%d\n", *varPtr); // // обращаемся по адресу в памяти и выводим значение
 
-    free(varPtr); // освободить память
-    varPtr = NULL;
+        free(varPtr); // освободить память
+        varPtr = NULL;
+    }
 
+
+    void *nonTypePtr = NULL; // не типизированный указатель
+
+
+    tasks();
     return 0;
+}
+
+
+void tasks(int a)
+{
+    /*
+    Создайте несколько указателей разных типов и выделите для них память. 
+    Далее присвойте им значения и выведите на экран.
+    Помните про очистку памяти и проверку наличия адреса.
+    */
+    
+    int *intVarPtr = NULL;
+    intVarPtr = malloc(sizeof(int));
+    *intVarPtr = 25;
+
+    float *floatVarPtr = NULL;
+    floatVarPtr = malloc(sizeof(float));
+    *floatVarPtr = 1.7;
+
+    char *charVarPtr = NULL;
+    charVarPtr = malloc(sizeof(char));
+    *charVarPtr = 'q';
+
+    if (intVarPtr != NULL && floatVarPtr != NULL && charVarPtr !=NULL)
+    {
+        printf("%d, %0.1f, %c", 
+        *intVarPtr, *floatVarPtr, *charVarPtr);
+
+        free(intVarPtr);
+        intVarPtr = NULL;
+
+        free(floatVarPtr);
+        floatVarPtr = NULL;
+
+        free(charVarPtr);
+        charVarPtr = NULL;
+    } 
+    
+
+
+
 }
