075a73d5aa7eda689bc89ffe8b0eed3697daddd0
Iaroslav Os <osmnbx@gmail.com>
2022-09-18 11:48:52 +0300

    added/updated

--- c002basics3/c021_pointers_null.c
+++ c002basics3/c021_pointers_null.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void tasks();
+
 int main()
 {
     int num; // переменная
@@ -10,15 +12,39 @@ int main()
     num = 5; // значение переменной
     *numPtr = 2; // изменяем значение через указатель (num изменился на 12)
 
-    numPtr = NULL; // указатель указывает никуда
+    // numPtr = NULL; // указатель указывает никуда
     if (numPtr != NULL) 
+    {
         *numPtr = 7; // изменить значение если в указателе содержится адрес
+        printf("%d\n", numPtr); // вывести адрес указателя
+        printf("%d\n", *numPtr); // вывести значение находящееся по адресу указателя (разыменование)
+    }
 
     // при объявлении указателя рекомендуется сразу присваивать ему значение NULL
 
     printf("%d\n", num);
-    printf("%d\n", numPtr); // вывести адрес указателя
-    printf("%d\n", *numPtr); // вывести значение находящееся по адресу указателя (разыменование)
 
+    tasks();
     return 0;
+}
+
+
+void tasks()
+{
+    float value1 = 0.1;
+    float value2 = 0.2;
+    printf("value1 = %0.1f\n", value1);
+    printf("value2 = %0.1f\n", value2);
+
+    float *valuePtr = NULL;
+    valuePtr = &value1;
+    if (valuePtr != NULL)
+    {
+        *valuePtr = 0.3;
+        valuePtr = &value2;
+        *valuePtr = 0.4;
+    }
+ 
+    printf("value1 = %0.1f\n", value1);
+    printf("value2 = %0.1f\n", value2);
 }
