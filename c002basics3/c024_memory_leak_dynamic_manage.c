0f739ddf7b9927d601aa8a06be53c94783f27c86
Iaroslav Os <osmnbx@gmail.com>
2022-09-18 17:33:19 +0300

    added/updated

--- c002basics3/c024_memory_leak_dynamic_manage.c
+++ c002basics3/c024_memory_leak_dynamic_manage.c
@@ -5,7 +5,7 @@ int main()
 {
     int x;
 
-    // sizeof сколько байт занимает тот или иной тип
+    // функция sizeof показывает сколько байт занимает тот или иной тип данных
 
     x = sizeof(int);
     printf("%d\n", x); // 4 байта, 32 бита
@@ -19,8 +19,11 @@ int main()
     x = sizeof(char);
     printf("%d\n", x); // 1 байт, 8 бит
 
-    char *somePointer = NULL;
-    printf("%d\n", sizeof(somePointer)); // 8 байт, 64 бита
+    char *somePtr = NULL;
+    int pointerSize = sizeof(somePtr);
+    printf("%d\n", pointerSize); // 8 байт, 64 бита
+
+    // malloc -  
 
     return 0;
 }
