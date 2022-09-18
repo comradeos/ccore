07bced6ae13626f7aa4fd09311a976a8e0d60152
Iaroslav Os <osmnbx@gmail.com>
2022-09-18 12:15:16 +0300

    added/updated

--- c002basics3/c022_pass_params_via_pointers.c
+++ c002basics3/c022_pass_params_via_pointers.c
@@ -3,9 +3,25 @@
 
 void tasks();
 
+
+int inc(int a)
+{
+    return a + 1;
+}
+
+
+void incByPointer(int *x)
+{
+    *x = *x + 1; // * - оператор обращения по адресу
+}
+
 int main()
 {
-   
+    int i = 7;
+    i = inc(i);
+    incByPointer(&i); // & - операция взятия адреса
+    
+    printf("%d\n", i);
     return 0;
 }
 
