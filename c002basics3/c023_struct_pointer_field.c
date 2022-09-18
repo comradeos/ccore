9762c1b7a8cf68608cf95ee71ffc2170bbea2bb2
Iaroslav Os <osmnbx@gmail.com>
2022-09-18 14:15:35 +0300

    added/updated

--- c002basics3/c023_struct_pointer_field.c
+++ c002basics3/c023_struct_pointer_field.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct SPoint TPoint;
+
+struct SPoint {
+    int a, b;
+};
+
+
+
 int main()
 {
+    TPoint point;
+    point.a = 123;
+    printf("%d\n", point.a);
     return 0;
 }
