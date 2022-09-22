8bda9df13a15a90918a7f3681c2ddb8894464a07
Iaroslav Os <osmnbx@gmail.com>
2022-09-22 09:35:01 +0300

    added/updated

--- c002basics3/c025_dynamic_arrays_addressing_memory.c
+++ c002basics3/c025_dynamic_arrays_addressing_memory.c
@@ -15,5 +15,16 @@ int main(int argc, char const *argv[])
     printf("%d\n", mas[0]);
     printf("%d\n", mas[1]);
 
+
+    // динамический массив (darr - dynamic array)
+    int *darr = NULL; // создаем пустой указатель
+    darr = malloc(sizeof(int)*2); // для int массива размерностью 2 выделяем в 2 раза больше памяти чем нужно для int
+    
+    darr[0] = 11;
+    darr[1] = 22;
+    
+    printf("%d\n", darr[0]);
+    printf("%d\n", darr[1]);
+
     return 0;
 }
