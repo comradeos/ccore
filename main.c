c0c2e4ef600e00a95296ad6bdf9aed587b018ef6
Iaroslav Os <osmnbx@gmail.com>
2022-09-24 10:53:35 +0300

    added/updated

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
 #include "c002basics3/c026_another_source.h"
 
 int main() {
     printf("Hello, World!\n");
-    c026_another_source();
+//    c026_another_source();
     return 0;
 }
