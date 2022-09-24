4c56778432d14e880e6615e8be5ec481fc7d0b04
Iaroslav Os <osmnbx@gmail.com>
2022-09-24 10:25:57 +0300

    jb commit test

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "mclib.h"
 
 int main() {
     printf("Hello, World!\n");
+    main1();
     return 0;
 }
---
+++ mclib.h
@@ -0,0 +1,8 @@
+#ifndef CCORE_MCLIB_H
+#define CCORE_MCLIB_H
+
+#endif //CCORE_MCLIB_H
+
+void main1() {
+    printf("Hello from mclib.h\n");
+}
