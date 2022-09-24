48be4ac8d0352c44db43d20d668633874afb2f37
Iaroslav Os <osmnbx@gmail.com>
2022-09-24 11:11:26 +0300

    added/updated

--- c002basics3/c026_pointer_to_pointer.h
+++ c002basics3/c026_pointer_to_pointer.h
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 void c026_pointer_to_pointer() {
-    int k = 777;
-    printf("%d\n",k);
+    int k = 777; // variable
+    int *p = &k; // pointer to var
+    *p = 111; // change value using pointer
+
+
+    printf("%d\n", k);
 }
