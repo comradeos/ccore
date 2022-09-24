ead9fe9f0ae16cecbc1570371454b394c4ef1a82
Iaroslav Os <osmnbx@gmail.com>
2022-09-24 15:30:20 +0300

    added/updated

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "c002basics3/c026_pointer_to_pointer.h"
+#include "c002basics3/c027_tasks_21_22_23.h"
 
 int main() {
-    c026_pointer_to_pointer();
+//    c026_pointer_to_pointer();
+    c027_tasks_21_22_23();
     return 0;
 }
