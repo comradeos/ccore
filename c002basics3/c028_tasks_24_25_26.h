bd2db1aeee038d3afce205175709978df9f8227d
Iaroslav Os <osmnbx@gmail.com>
2022-09-24 17:01:31 +0300

    added/updated

--- c002basics3/c028_tasks_24_25_26.h
+++ c002basics3/c028_tasks_24_25_26.h
@@ -1,13 +1,34 @@
+struct SMyPoint {
+    int x,y;
+};
+
+typedef struct SMyPoint TMyPoint;
+
 void c028_tasks_24_25_26() {
     /* 24
     1. Создайте несколько указателей разных типов и выделите для них память.
     Далее присвойте им значения и выведите на экран.
     Помните про очистку памяти и проверку наличия адреса.
     */
-    double * floatPtr = NULL;
-    floatPtr = malloc(sizeof (double));
-    *floatPtr = 231.32;
-    printf("%0.2f\n", *floatPtr);
+    double * doublePtr = NULL;
+    doublePtr = malloc(sizeof (double));
+    *doublePtr = 231.32;
+    printf("%0.2f\n", *doublePtr);
+
+    // тоже самое но со структурой
+    TMyPoint * pointer = NULL;
+    pointer = malloc(sizeof (TMyPoint));
+    (*pointer).x = 2;
+    (*pointer).y = 3;
+    printf("%d, %d\n", (*pointer).x, (*pointer).y);
+
+    // попробуем для указателя на void (нетипизированный указатель)
+    void *unkType = NULL;
+    unkType = malloc(8); // выделяем ему 8 байт
+    *(int*) unkType = 777; // приводим тип указателя void к типу указателя int
+    printf("%d\n", *(int*) unkType);
+
+
 
     /* 25
     1. Создайте динамический массив целого типа. Выделите ему память на 10 элементов.
