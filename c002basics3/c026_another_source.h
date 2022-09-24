d2cc9f9930b884b6adca42dc0419754c7818d985
Iaroslav Os <osmnbx@gmail.com>
2022-09-24 11:00:59 +0300

    added/updated

--- c002basics3/c026_another_source.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void c026_another_source() {
-    printf("hello from c026_another_source.c\n");
-}
---
+++ c002basics3/c026_pointer_to_pointer.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void c026_pointer_to_pointer() {
+    int k = 777;
+    printf("%d\n",k);
+}
