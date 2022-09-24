d4c16c0114a8a0c879e54d9fb1b167d6ed78eff2
Iaroslav Os <osmnbx@gmail.com>
2022-09-24 11:15:26 +0300

    added/updated

--- c002basics3/c026_pointer_to_pointer.h
+++ c002basics3/c026_pointer_to_pointer.h
@@ -6,6 +6,10 @@ void c026_pointer_to_pointer() {
     int *p = &k; // pointer to var
     *p = 111; // change value using pointer
 
+    int **p2 = &p; // set p2 = address of p
+    **p2 = 333;
+
+
 
     printf("%d\n", k);
 }
