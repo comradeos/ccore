a17ca87a1af0be86b341895e4a58aa0825c0eb7c
Iaroslav Os <osmnbx@gmail.com>
2022-09-24 10:35:11 +0300

    updated

---
+++ c002basics3/c026_another_source.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void c026_another_source() {
+    printf("hello from c026_another_source.c\n");
+}
--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include "mclib.h"
+#include "c002basics3/c026_another_source.h"
 
 int main() {
     printf("Hello, World!\n");
-    main1();
+    c026_another_source();
     return 0;
 }
