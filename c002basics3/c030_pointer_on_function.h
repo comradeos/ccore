5329d4a8c084f6c62d7c7576d4f5c0fe2c0bed21
Iaroslav Os <osmnbx@gmail.com>
2022-09-25 16:21:49 +0300

    added/updated

---
+++ c002basics3/c030_pointer_on_function.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void setB321(int *b) {
+    *b = 321;
+}
+void setArray0(int * arr) {
+    arr[0] = 222;
+}
+
+void c030_pointer_on_function() {
+    int b = 9;
+    setB321(&b);
+    printf("%d", b);
+
+    int arr[5] = {1,2,3,4,5};
+    setArray0(arr);
+    printf("%d\n", arr[0]);
+}
--- main.c
+++ main.c
@@ -2,11 +2,13 @@
 #include "c002basics3/c027_tasks_21_22_23.h"
 #include "c002basics3/c028_tasks_24_25_26.h"
 #include "c002basics3/c029_game_platformer.h"
+#include "c002basics3/c030_pointer_on_function.h"
 
 int main() {
 //    c026_pointer_to_pointer();
 //    c027_tasks_21_22_23();
 //    c028_tasks_24_25_26();
-    c029_game_platformer();
+//    c029_game_platformer();
+    c030_pointer_on_function();
     return 0;
 }
