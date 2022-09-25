f66ebae3d1d24695eeddb4858c7df0edbf6cd03d
Iaroslav Os <osmnbx@gmail.com>
2022-09-25 20:15:49 +0300

    added/updated

--- c002basics3/c030_pointer_on_function.h
+++ c002basics3/c030_pointer_on_function.h
@@ -3,13 +3,22 @@
 
 
 // делаем тип переменной которая будет указазателем на функцию
-typedef int (*TMaxAB)(int, int);
+typedef int (*TFuncAB)(int, int);
 
 int get5() {
     return 5;
 }
 
 int maxAB(int a, int b) {
+    if (a > b) {
+        return a;
+    } else if (b > a) {
+        return b;
+    }
+    return 0;
+}
+
+int mixAB(int a, int b) {
     if (a > b) {
         return a;
     } else if (b > a) {
@@ -32,4 +41,8 @@ void c030_pointer_on_function() {
     int (*func2)(int, int);
     func2 = maxAB;
     printf("%d\n", func2(7,9));
+
+    TFuncAB func3;
+    func3 = maxAB;
+    printf("%d\n", func3(74,9));
 }
