bc8efd31341a9804193668b014d88796ae1e7b53
Iaroslav Os <osmnbx@gmail.com>
2022-09-27 13:04:41 +0300

    added/updated

--- c002basics3/c032_switch_static.h
+++ c002basics3/c032_switch_static.h
@@ -9,12 +9,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 int getInt() {
     static int i = 0;
     return i++;
 }
+
+void c032Tasks();
 
 void c032_switch_static() {
     int val = 3;
@@ -37,4 +40,39 @@ void c032_switch_static() {
     printf("%d\n", getInt());
     printf("%d\n", getInt());
     printf("%d\n", getInt());
+    printf("\n");
+
+    c032Tasks();
+}
+
+void c032Tasks() {
+    /*
+    * при помощи генератора случайных чисел и оператора switch напишите программу,
+    * которая будет советовать вам, какой фильм сегодня посмотреть.
+    */
+    srand(time(NULL));
+    int num = (rand() % (5 - 1 + 1)) + 1;
+
+    switch (num) {
+        case 1:
+            printf("Rick and Morty\n");
+            break;
+        case 2:
+            printf("Adventure Time\n");
+            break;
+        case 3:
+            printf("Gravity Falls\n");
+            break;
+        case 4:
+            printf("Family Guy\n");
+            break;
+        case 5:
+            printf("South Park\n");
+            break;
+        default:
+            printf("I don't know\n");
+            break;
+    }
+
+
 }
