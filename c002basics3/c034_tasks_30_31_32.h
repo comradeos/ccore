3b2832d864ba1728b7ab0044e760d084db3e6f85
Iaroslav Os <osmnbx@gmail.com>
2022-09-28 19:45:24 +0300

    added/updated

---
+++ c002basics3/c034_tasks_30_31_32.h
@@ -0,0 +1,16 @@
+//
+// Created by Admin on 9/28/2022.
+//
+
+#ifndef CCORE_C034_TASKS_30_31_32_H
+#define CCORE_C034_TASKS_30_31_32_H
+
+#endif //CCORE_C034_TASKS_30_31_32_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void c034_tasks_30_31_32() {
+
+}
