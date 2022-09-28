1cfff1e0cc283013d0eb786ee67680292279d5e5
Iaroslav Os <osmnbx@gmail.com>
2022-09-28 19:54:33 +0300

    added/updated

--- c002basics3/c034_tasks_30_31_32.h
+++ c002basics3/c034_tasks_30_31_32.h
@@ -28,7 +28,7 @@ void c034_tasks_30_31_32() {
     */
     double (* myFunction)(double, double);
     myFunction = f1;
-    printf("%0.2f\n", myFunction(1.2, 3.4));
+    printf("%0.2f\n", myFunction(5.2, 5.4));
     myFunction = f2;
-    printf("%0.2f\n", myFunction(1.2, 3.4));
+    printf("%0.2f\n", myFunction(5.2, 5.4));
 }
