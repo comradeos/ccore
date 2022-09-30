718715971db54a8ec162fef3b6b98f01d6151399
Iaroslav Os <osmnbx@gmail.com>
2022-09-30 10:30:08 +0300

    added/updated

---
+++ c002basics3/c036_work_with_files.h
@@ -0,0 +1,13 @@
+//
+// Created by Admin on 9/30/2022.
+//
+
+#ifndef CCORE_C036_WORK_WITH_FILES_H
+#define CCORE_C036_WORK_WITH_FILES_H
+
+#endif //CCORE_C036_WORK_WITH_FILES_H
+
+
+void c036_work_with_files() {
+
+}
