954d0996deb39a2e382411b0f3f5d7b05349200e
Iaroslav Os <osmnbx@gmail.com>
2022-10-15 18:32:37 +0300

    add/upd

--- c002basics3/c041_files_read_write_position.h
+++ c002basics3/c041_files_read_write_position.h
@@ -8,5 +8,11 @@
 #endif //C002BASICS3_C041_FILES_READ_WRITE_POSITION_H
 
 void c041_files_read_write_position() {
-    printf("c041_files_read_write_position\n");
+
+    FILE *f;
+    f = fopen("c041_file_1.txt", "w");
+        fprintf(f, "hello file\n");
+    fclose(f);
+
+
 }
