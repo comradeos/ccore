9908ac5a325bd25a98d1b8bc869c32af796b5539
Iaroslav Os <osmnbx@gmail.com>
2023-06-20 13:38:07 +0200

    updated

--- c004drchuck/chapter0.h
+++ c004drchuck/chapter0.h
@@ -12,15 +12,21 @@
 
 void chapter0() {
     printf("chapter0: \n");
+    // ALGOL 60 (Algorithmic Language 1960)
+    // BASIC (Beginners' All-purpose Symbolic Instruction Code) 1964
+    // BCPL (Basic Combined Programming Language) 1967 / B 1969
+    // PASCAL 1970
+    // C 1972
+    // C++ 1985
 
     // malloc() and free()
 
     char inputBuffer[1024]; // size of input buffer
-    char tmp[8];
+    char * tmp;
 
     scanf("%s", inputBuffer);
     printf("%s\n", inputBuffer);
 
-    long long int number = strtoll(inputBuffer, (char **) tmp, 10);
+    long long int number = strtoll(inputBuffer, &tmp, 10);
     printf("number is: %lld", number);
 }
