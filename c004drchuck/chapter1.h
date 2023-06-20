4329b259192828da730440e072f6983eb63459e9
Iaroslav Os <osmnbx@gmail.com>
2023-06-20 17:29:24 +0200

    updated

--- c004drchuck/chapter1.h
+++ c004drchuck/chapter1.h
@@ -12,13 +12,9 @@
 void chapter1() {
     printf("chapter1: \n");
 
-    int bottom = 0;
-    int top = 300;
-    int step = 20;
-
     printf("Farenheight | Celsius\n");
 
-    for (int i=bottom; i<=top; i=i+step) {
+    for (int i = 0; i <= 300; i = i+20) {
         printf("%11d | %7.1f\n", i, (i - 32.0) * (5.0/9.0));
     }
 }
