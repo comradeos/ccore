42267ca64d2297edfef582ae32ad3f1d50be66fc
Iaroslav Os <osmnbx@gmail.com>
2023-06-20 14:38:42 +0200

    updated

---
+++ c004drchuck/chapter1.h
@@ -0,0 +1,15 @@
+//
+// Created by comradeos on 20/06/2023.
+//
+
+#ifndef C004DRCHUCK_CHAPTER1_H
+#define C004DRCHUCK_CHAPTER1_H
+
+#endif //C004DRCHUCK_CHAPTER1_H
+
+#include "stdio.h"
+
+void chapter1() {
+    printf("chapter1: \n");
+
+}
--- c004drchuck/main.c
+++ c004drchuck/main.c
@@ -2,10 +2,19 @@
 
 #include <stdio.h>
 #include "chapter0.h"
+#include "chapter1.h"
+//#include "chapter2.h"
+//#include "chapter3.h"
+//#include "chapter4.h"
+//#include "chapter5.h"
+//#include "chapter6.h"
+//#include "chapter7.h"
+//#include "chapter8.h"
 
 int main() {
-    printf("https://www.youtube.com/watch?v=j-_s8f5K30I\n");
-    chapter0();
+//    printf("https://www.youtube.com/watch?v=j-_s8f5K30I\n");
+//    chapter0();
+    chapter1();
 
     return 0;
 }
