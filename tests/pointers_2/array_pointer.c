3f01dc3e9272f671ef7dabee3a71cb5f0096eca1
Iaroslav Os <osmnbx@gmail.com>
2023-06-25 15:24:11 +0200

    Update array_pointer.c

--- tests/pointers_2/array_pointer.c
+++ tests/pointers_2/array_pointer.c
@@ -5,6 +5,8 @@
 int main() {
     int nums[] = { 5, 7, 1 };
     int * p_nums = nums;
-    printf("nums[0]: %d\n", * (p_nums + 2));
+    printf("nums[0]: %d\n", * (p_nums + 0)); // 5
+    printf("nums[1]: %d\n", * (p_nums + 1)); // 7
+    printf("nums[2]: %d\n", * (p_nums + 2)); // 1
     return 0;
 }
