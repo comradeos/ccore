0447ee7ffdb011e359e959e3648c7db00fe9c0c0
Iaroslav Os <osmnbx@gmail.com>
2023-06-25 15:21:31 +0200

    Update array_pointer.c

--- tests/pointers_2/array_pointer.c
+++ tests/pointers_2/array_pointer.c
@@ -3,9 +3,8 @@
 #include "string.h"
 
 int main() {
-    char arr[] = "abc";
-    char * p_arr = arr;
-
-    printf("%s\n", *p_arr);
+    int nums[] = { 5, 7, 1 };
+    int * p_nums = nums;
+    printf("nums[0]: %d\n", * (p_nums + 2));
     return 0;
 }
