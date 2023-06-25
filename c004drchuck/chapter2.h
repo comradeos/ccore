58ec126106ee221d9aee43aa98d4bdaaa5a3650f
Iaroslav Os <osmnbx@gmail.com>
2023-06-25 09:09:16 +0200

    updated

--- c004drchuck/chapter2.h
+++ c004drchuck/chapter2.h
@@ -6,11 +6,17 @@ void printDataTypes() {
     printf("myChar %c\n", myChar);
 
     int myInt = 7;
-    float myFloat = 2.49;
-    double myDouble = 923.2;
+    printf("myInt %d\n", myInt);
+
+    float myFloat = (float)2.49;
+    printf("myFloat %.2f\n", myFloat);
+
+    double myDouble = 923.2231;
+    printf("myDouble %.4f\n", myDouble);
 
 }
 
 void chapter2() {
     printf("chapter2: \n");
+    printDataTypes();
 }
