cf90399d3e578322dc5b37cd5e91c29ffbf04a20
Iaroslav Os <osmnbx@gmail.com>
2023-06-25 15:35:11 +0200

    updated

--- tests/pointers_2/array_pointer.c
+++ tests/pointers_2/array_pointer.c
@@ -5,8 +5,16 @@
 int main() {
     int nums[] = { 5, 7, 1 };
     int * p_nums = nums;
+    
     printf("nums[0]: %d\n", * (p_nums + 0)); // 5
     printf("nums[1]: %d\n", * (p_nums + 1)); // 7
     printf("nums[2]: %d\n", * (p_nums + 2)); // 1
+
+    char words[][10] = { "word1", "word2", "word3" }; // 10 - max len of word
+    char * p_words = (char *) words;
+    printf("words[0]: %s\n", words[0]);
+    printf("words[0]: %s\n", p_words[1]);
+
+    printf("ok\n");
     return 0;
 }
