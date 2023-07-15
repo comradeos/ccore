4d41b0441e71afbdd2201c0959502c93a0de33ea
Iaroslav Os <osmnbx@gmail.com>
2023-07-15 20:53:13 +0200

    Update menu.c

--- tests/StructInStruct/menu.c
+++ tests/StructInStruct/menu.c
@@ -10,15 +10,28 @@ int menu_size = 0;
 
 void addMenuItem(char * name) {
     menu[menu_size].name = name;
+    ++menu_size;
+}
+
+void addSubMenuItem(int index, char * name) {
+    MenuItem_t sm;
+    sm.name = name;
+    menu[index].menuItem = &sm;
 }
 
 void printMenu() {
     for (int i=0; i<=menu_size; ++i) {
         printf("%s\n", menu[i].name);
+        if (menu[i].menuItem != NULL) {
+            printf("-> %s\n", menu[i].menuItem->name);
+        }
     }
 }
 
 int main() {
     addMenuItem("item1");
+    addSubMenuItem(0, "subItem1");
+    addMenuItem("item2");
+
     printMenu();
 }
