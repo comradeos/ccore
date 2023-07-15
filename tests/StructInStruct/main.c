94a7c89bc31d0321b607c4deec163bc4e28ffc6d
Iaroslav Os <osmnbx@gmail.com>
2023-07-15 18:00:42 +0200

    updated

--- tests/StructInStruct/main.c
+++ tests/StructInStruct/main.c
@@ -1,5 +1,12 @@
 #include "stdio.h"
 
+typedef struct {
+    int number;
+} MyStruct_t;
+
+
 int main() {
-    printf("hello\n");
+    MyStruct_t ms;
+    ms.number = 33;
+    printf("hello, %d\n", ms.number);
 }
