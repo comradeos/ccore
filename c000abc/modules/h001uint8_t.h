a823243eb454e8a6da605c9d250cd0567a9be7ea
Iaroslav Os <osmnbx@gmail.com>
2023-09-13 18:59:30 +0300

    updated

--- c000abc/modules/h001uint8_t.h
+++ c000abc/modules/h001uint8_t.h
@@ -7,5 +7,9 @@ void h001uint8_t() {
     uint8_t num2 = 256; // 0 -> overflow
 
     printf("%d\n", num);
-}
+
+    int8_t num3 = -128; // -128 - 127
+    int8_t num4 = 128; // -128 -> overflow
+
+    printf("%d\n", num3);
 }
