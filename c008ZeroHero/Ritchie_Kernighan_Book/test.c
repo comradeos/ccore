0cf84ea903df1204edb5f988e91ba6b6b9450d0d
Iaroslav Os <osmnbx@gmail.com>
2026-04-24 07:01:29 +0300

    Add test.c and test executable
    Add a small C test program (test.c) and its compiled executable (test). The program demonstrates printing an int as a char and as a digit (num and num+'0'), then reads stdin and prints each non-newline character as [ascii -> char]. The source includes a suggested build/run command (gcc -Wall -o ./test ./test.c && ./test).

---
+++ c008ZeroHero/Ritchie_Kernighan_Book/test.c
@@ -0,0 +1,31 @@
+// gcc -Wall -o ./test ./test.c && ./test
+
+#include <stdio.h>
+
+int main()
+{
+    int num = 3;
+
+    printf("---------------\n");
+    printf("%c\n", num);
+    printf("%c\n", num + '0');
+    printf("---------------\n");
+
+
+    printf("TEST INPUT: ");
+
+    int c; 
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == 10)
+        {
+            continue;
+        }
+
+        printf("[%d -> %c] ", c, c);
+    }
+
+    return 0;
+}
+
